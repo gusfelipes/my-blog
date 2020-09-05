f7779b8385904d33238a4cf323515ea98f688c57
gustavo <gusfelipes@gmail.com>
2020-09-05 17:19:39 -0300

    add menu links component and update side bar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Profile from "../Profile"
 import SideBar from "../SideBar"
 import GlobalStyles from "../../styles/global"
 
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,11 +1,16 @@
 import React from "react"
+
 import Profile from "../Profile"
+import SocialLinks from "../SocialLinks"
+import MenuLinks from "../MenuLinks"
 
 import * as S from "./styled"
 
 const SideBar = () => (
   <S.SideBarWrapper>
     <Profile />
+    <SocialLinks />
+    <MenuLinks />
   </S.SideBarWrapper>
 )
 
