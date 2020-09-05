c4ae5fe78bcdca8dd233b314265f77cef70033f1
gustavo <gusfelipes@gmail.com>
2020-09-05 17:19:04 -0300

    add social links component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,18 @@
+const user = "gusfelipes"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://www.github.com/${user}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${user}`,
+  },
+  {
+    label: "LinkedinSquare",
+    url: `https://www.linkedin.com/in/${user}`,
+  },
+]
+
+export default links
