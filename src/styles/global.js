9aa35b757c81cbcda1c4e7e07155ee942d7d0b11
gustavo <gusfelipes@gmail.com>
2020-09-06 16:08:03 -0300

    add css variables

--- src/styles/global.js
+++ src/styles/global.js
@@ -62,5 +62,16 @@ const GlobalStyles = createGlobalStyle`
   	width: 100%;
   	height: auto;
   }
+
+  body {
+    --borders: #38444d;
+    --texts: #8899a6;
+    --postColor: #fff;
+    --highlight: #1fa1f2;
+    --mediumBackground: #192734;
+    --background: #16202c;
+    --white: #fff;
+    --black: #222;
+  }
 `
 export default GlobalStyles
