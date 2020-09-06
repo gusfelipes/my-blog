b16c1303c62641c17e65c90df4400a9203ce31e2
gustavo <gusfelipes@gmail.com>
2020-09-06 14:36:36 -0300

    update configure angolia

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -8,35 +8,33 @@ const postsQuery = `{
                     slug
                 }
                 frontmatter {
-                    background
                     category
                     date_timestamp: date
                     date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
                     description
                     title
                 }
-                excerpt(pruneLenght:5000)
+                excerpt(pruneLength:5000)
             }
         }
     }
 }`
 
-const faltten = arr =>
+const flatten = arr =>
   arr.map(({ node: { frontmatter, ...rest } }) => ({
-    ...frontmattter,
+    ...frontmatter,
     date_timestamp: parseInt(
       (new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)
     ),
     ...rest,
   }))
+const settings = { attributesToSnippet: [`excerpt:20`] }
 const queries = [
   {
     query: postsQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
-    indexName: "Posts",
-    settings: {
-      attributesToSnippet: ["excerpt:20"],
-    },
+    indexName: `Posts`,
+    settings,
   },
 ]
 
